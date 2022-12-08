9d35487183b07dcee8f7fbdc06cce6154aeacf1f
chevonnelise <114272460+chevonnelise@users.noreply.github.com>
2022-12-08 08:50:31 +0000

    add taxi points api

--- script.js
+++ script.js
@@ -21,6 +21,21 @@ let circle = L.circle([1.3521, 103.8198], {
 
 circle.addTo(map);
 
+function getRandomLatLng(map) {
+    // get the boundaries of the map
+    let bounds = map.getBounds();
+    let southWest = bounds.getSouthWest();
+    let northEast = bounds.getNorthEast();
+    let lngSpan = northEast.lng - southWest.lng;
+    let latSpan = northEast.lat - southWest.lat;
+
+    let randomLng = Math.random() * lngSpan + southWest.lng;
+    let randomLat = Math.random() * latSpan + southWest.lat;
+
+    return [ randomLat, randomLng,];
+}
+
+// create marker cluster
 let markersClusterLayer = L.markerClusterGroup();
 
 for (let i = 0; i < 1000; i++) {
